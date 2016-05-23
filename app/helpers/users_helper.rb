27162c18f0839940f0920c675fc6e9cee4bcbe10
= <joeyzmarion@gmail.com>
2016-05-22 20:16:24 -0500

    Finish user edit, update, index, and destroy actions.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 })
     email_digest = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{email_digest}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{email_digest}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar")
   end
 end
