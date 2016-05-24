c426277d79eaeb8ee017a9960e2eff462e408698
= <joeyzmarion@gmail.com>
2016-05-24 17:30:05 -0500

    Add content to topic#show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
   before_action :get_user,       only: [:new, :create, :edit, :update]
-  before_action :get_forum,      only: [:new, :create]
+  before_action :get_forum,      only: [:new, :create, :show]
 
   def index
     @topics = Topic.where(forum_id: params[:forum_id]).order("created_at DESC")
